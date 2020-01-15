6137862f08d68c3e1040b90b9e77fcfa1e14b0ca
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-01-15 10:25:26 -0700

    Update InitalClass.java

--- src/InitalClass.java
+++ src/InitalClass.java
@@ -2,11 +2,12 @@ public class InitalClass {
 
 	public static void main(String[] args) {
 		
+		InitalClass aObj = new InitalClass();
 		System.out.println("This is inital commits");
-		
+		aObj.Calc();
 	}//end of main
 
-	public static void Calc()
+	public void Calc()
 	{
 		System.out.println("x =" + 10);
 	}
