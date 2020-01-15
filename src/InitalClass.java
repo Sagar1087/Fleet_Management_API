dbe0a6abc3cc24873586bb613272868a116033e7
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-01-15 10:24:27 -0700

    Update IntialClass

--- src/InitalClass.java
+++ src/InitalClass.java
@@ -3,7 +3,11 @@ public class InitalClass {
 	public static void main(String[] args) {
 		
 		System.out.println("This is inital commits");
+		
+	}//end of main
 
+	public static void Calc()
+	{
+		System.out.println("x =" + 10);
 	}
-
 }
