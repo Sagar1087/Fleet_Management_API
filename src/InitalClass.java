220900c31a730a467da0965f3c337263723d5d79
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-01-15 10:29:34 -0700

    Update InitalClass.java

--- src/InitalClass.java
+++ src/InitalClass.java
@@ -5,10 +5,16 @@ public class InitalClass {
 		InitalClass aObj = new InitalClass();
 		System.out.println("This is inital commits");
 		aObj.Calc();
+		NewFeatureMethod();
 	}//end of main
 
 	public void Calc()
 	{
 		System.out.println("x =" + 10);
+	}
+	
+	public static void NewFeatureMethod() {
+		
+		System.out.println("Building new management feature!");
 	}
 }
